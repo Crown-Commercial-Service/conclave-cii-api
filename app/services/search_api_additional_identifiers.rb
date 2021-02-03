a6ff95b6a8ec8ccc4fb3f1aecc23a7511626d8cd
Mo <mo-zag@users.noreply.github.com>
2021-02-03 13:55:55 +0000

    Update search_api_additional_identifiers.rb
    Fixed rubocop space issues

--- app/services/search_api_additional_identifiers.rb
+++ app/services/search_api_additional_identifiers.rb
@@ -12,7 +12,7 @@ class SearchApiAdditionalIdentifiers
     when Common::AdditionalIdentifier::SCHEME_COMPANIES_HOUSE
       @result = get_companies_house_additional(@organisation_id)
     when Common::AdditionalIdentifier::SCHEME_ENG_WALES_CHARITY, Common::AdditionalIdentifier::SCHEME_NORTHEN_IRELAND_CHARITY, Common::AdditionalIdentifier::SCHEME_SCOTISH_CHARITY
-      @result =  get_charity_additional(@organisation_id, @scheme_id)
+      @result = get_charity_additional(@organisation_id, @scheme_id)
     end
 
     @result if @result.present?
