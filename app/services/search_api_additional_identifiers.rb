d6893f54996edac5adc8c5cf9aa2a5c16f469711
Mo <mo-zag@users.noreply.github.com>
2021-02-03 13:49:39 +0000

    Update search_api_additional_identifiers.rb

--- app/services/search_api_additional_identifiers.rb
+++ app/services/search_api_additional_identifiers.rb
@@ -10,11 +10,9 @@ class SearchApiAdditionalIdentifiers
   def call
     case @scheme_id
     when Common::AdditionalIdentifier::SCHEME_COMPANIES_HOUSE
-      @result = get_companies_house(@organisation_id)
+      @result = get_companies_house_additional(@organisation_id)
     when Common::AdditionalIdentifier::SCHEME_ENG_WALES_CHARITY, Common::AdditionalIdentifier::SCHEME_NORTHEN_IRELAND_CHARITY, Common::AdditionalIdentifier::SCHEME_SCOTISH_CHARITY
-      @result =  get_charity(@organisation_id, @scheme_id)
-    when Common::AdditionalIdentifier::SCHEME_DANDB
-      @result =  get_duns(@organisation_id)
+      @result =  get_charity_additional(@organisation_id, @scheme_id)
     end
 
     @result if @result.present?
@@ -22,11 +20,11 @@ class SearchApiAdditionalIdentifiers
 
   private
 
-  def get_companies_house(company_reg_number)
+  def get_companies_house_additional(company_reg_number)
     CompaniesHouse::AdditionalIdentifier.new(company_reg_number).build_response
   end
 
-  def get_charity(chartity_number, scheme_id)
+  def get_charity_additional(chartity_number, scheme_id)
     FindThatCharity::AdditionalIdentifier.new(chartity_number, scheme_id).build_response
   end
 end
