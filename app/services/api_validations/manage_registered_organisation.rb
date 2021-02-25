9fd1e633bb30f3f3d64f6ee1fb02f9e82dc23a51
Mo <mo-zag@users.noreply.github.com>
2021-02-25 20:08:15 +0000

    Update manage_registered_organisation.rb

--- app/services/api_validations/manage_registered_organisation.rb
+++ app/services/api_validations/manage_registered_organisation.rb
@@ -6,7 +6,7 @@ module ApiValidations
     attr_reader :data
 
     validates_presence_of :ccs_org_id, presence: true
-    validate :validate_ccs_org_id
+    validate :validate_registered_ccs_org_id
 
     # used to send response relevant http status code to user
     # if validation fails.
@@ -25,7 +25,7 @@ module ApiValidations
       data[key]
     end
 
-    def validate_ccs_org_id
+    def validate_registered_ccs_org_id
       validate = OrganisationSchemeIdentifier.find_by(ccs_org_id: @data[:ccs_org_id].to_s)
       errors.add(:ccs_org_id_not_found, '') if validate.blank?
     end
