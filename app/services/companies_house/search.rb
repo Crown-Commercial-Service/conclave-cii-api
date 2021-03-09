df5bfc4d6b9d1f9392567df0610beeebba79fa91
Tom Berey <tomberey1@gmail.com>
2021-03-09 12:58:16 +0000

    Fix COH to return only active status companies

--- app/services/companies_house/search.rb
+++ app/services/companies_house/search.rb
@@ -14,7 +14,7 @@ module CompaniesHouse
       resp = conn.get("/company/#{@company_reg_number}")
       @result = ActiveSupport::JSON.decode(resp.body) if resp.status == 200
 
-      if resp.status == 200 && @result.key?('primaryTopic') && @result['primaryTopic']['CompanyStatus'] != false
+      if resp.status == 200 && @result.key?('company_status') && @result['company_status'] == 'active'
         build_response
       else
         false
