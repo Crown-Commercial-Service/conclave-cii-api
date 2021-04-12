c5c9dd59fd4e2b69ed836171e3a1b58f5d9fb879
Mo <mo-zag@users.noreply.github.com>
2021-04-12 11:07:23 +0100

    disabled salesforce

--- app/controllers/api/v1/update_organisations_controller.rb
+++ app/controllers/api/v1/update_organisations_controller.rb
@@ -11,7 +11,8 @@ module Api
       def index
         result = search_scheme_api
 
-        Common::SalesforceHelper.new(result, params[:ccs_org_id]).insert_salesforce_record if result.present?
+        # Disabled until we rnew salesforce credentials
+        # Common::SalesforceHelper.new(result, params[:ccs_org_id]).insert_salesforce_record if result.present?
 
         save_or_update_organisation_scheme if result.present?
 
