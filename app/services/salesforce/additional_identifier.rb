2083f68e1ab6b630cf50d591af11fbd7f636cf49
Tom Berey <tomberey1@gmail.com>
2021-05-07 14:43:19 +0100

    CON-910-980-981/Two Salesforce IDs Added to CII and Change Active to Hidden

--- app/services/salesforce/additional_identifier.rb
+++ app/services/salesforce/additional_identifier.rb
@@ -9,44 +9,44 @@ module Salesforce
     end
 
     def build_response #
-      search_only_validated_primary ######1
-      find_from_additional_identifiers unless @found_record # calls method when @found_record = falsey #############1
+      search_only_validated_primary
+      find_from_additional_identifiers unless @found_record
       @results
     end
 
     private
 
-    def search_only_validated_primary ######2
+    def search_only_validated_primary
       salesforce_from_primary_scheme if @valid_schemes.include? @results[:identifier][:scheme]
     end
 
-    def search_only_validated_additional_identifiers(identifier) #############3
+    def search_only_validated_additional_identifiers(identifier)
       salesforce_from_additional_identifiers(identifier[:id], identifier[:scheme]) if @valid_schemes.include? identifier[:scheme]
     end
 
-    def get_salesforce(scheme_number, scheme_id) ######4 #############5
+    def get_salesforce(scheme_number, scheme_id)
       salesforce = Salesforce::Search.new(scheme_number, scheme_id)
       salesforce.fetch_results
     end
 
-    def salesforce_from_primary_scheme ######3
+    def salesforce_from_primary_scheme
       salesforce_result = get_salesforce(@results[:identifier][:id], @results[:identifier][:scheme])
       update_additional_identifiers(salesforce_result)
     end
 
-    def salesforce_from_additional_identifiers(id, scheme) #############4
+    def salesforce_from_additional_identifiers(id, scheme)
       salesforce_result = get_salesforce(id, scheme)
       update_additional_identifiers(salesforce_result)
     end
 
-    def update_additional_identifiers(salesforce_result) ######5 #############6
+    def update_additional_identifiers(salesforce_result)
       return unless salesforce_result.present? && @found_record == false
 
       @results[:additionalIdentifiers].push(salesforce_result)
       @found_record = true unless @results[:identifier][:scheme] == Common::AdditionalIdentifier::SCHEME_COMPANIES_HOUSE || Common::AdditionalIdentifier::SCHEME_DANDB # GB-COH or US-DUN.
     end
 
-    def find_from_additional_identifiers #############2
+    def find_from_additional_identifiers
       @results[:additionalIdentifiers].each do |identifier|
         search_only_validated_additional_identifiers(identifier) unless @found_record
       end
