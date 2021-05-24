512bdd9a4514d121623c153121cf3b155b8e16b6
Tom Berey <tomberey1@gmail.com>
2021-05-24 18:06:22 +0100

    sandbox-update

--- config/routes.rb
+++ config/routes.rb
@@ -23,20 +23,20 @@ Rails.application.routes.draw do
     end
   end
   # these are Mock testing endpoint will be removed on live
-  # namespace :api do
-  #   namespace :v1 do
-  #     namespace :mock do
-  #       get '/identities/schemes/organisation', to: 'organisations_mock#search_organisation'
-  #       get '/identities/schemes/identifiers', to: 'organisations_mock#search_organisation'
-  #       post '/identities/schemes/organisation', to: 'create_organisations_mock#index'
-  #       get '/identities/schemes', to: 'schemes_mock#schemes'
-  #       delete '/identities/schemes/organisation', to: 'remove_organisations_additional_identifier_mock#delete_additional_identifier'
-  #       delete '/identities/organisation', to: 'remove_organisations_mock#delete_organisation'
-  #       put '/identities/schemes/organisation', to: 'update_organisations_mock#index'
-  #       get '/identities/schemes/manageidentifiers', to: 'manage_organisations_mock#search_organisation'
-  #       get '/identities/schemes/organisations', to: 'registered_organisations_schemes_mock#search_organisation'
-  #       get '/identities/schemes/organisations/all', to: 'all_registered_organisations_schemes_mock#search_organisation'
-  #     end
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      namespace :mock do
+        get '/identities/schemes/organisation', to: 'organisations_mock#search_organisation'
+        get '/identities/schemes/identifiers', to: 'organisations_mock#search_organisation'
+        post '/identities/schemes/organisation', to: 'create_organisations_mock#index'
+        get '/identities/schemes', to: 'schemes_mock#schemes'
+        delete '/identities/schemes/organisation', to: 'remove_organisations_additional_identifier_mock#delete_additional_identifier'
+        delete '/identities/organisation', to: 'remove_organisations_mock#delete_organisation'
+        put '/identities/schemes/organisation', to: 'update_organisations_mock#index'
+        get '/identities/schemes/manageidentifiers', to: 'manage_organisations_mock#search_organisation'
+        get '/identities/schemes/organisations', to: 'registered_organisations_schemes_mock#search_organisation'
+        get '/identities/schemes/organisations/all', to: 'all_registered_organisations_schemes_mock#search_organisation'
+      end
+    end
+  end
 end
