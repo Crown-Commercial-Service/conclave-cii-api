7b08f651a75abd358ce2a6fc583968708f973b97
Tom Berey <tomberey1@gmail.com>
2021-06-07 17:17:35 +0100

    CON-851-Temporary-Fix-Until-GET-Endpoint-Returns-405

--- app/controllers/api/v1/mock/remove_organisations_additional_identifier_mock_controller.rb
+++ app/controllers/api/v1/mock/remove_organisations_additional_identifier_mock_controller.rb
@@ -1,17 +1,37 @@
 module Api
   module V1
     module Mock
-      class RemoveOrganisationsAdditionalIdentifierMockController < ApplicationMockController
-        before_action :remove_organisations_additional_identifier
+      class RemoveOrganisationsAdditionalIdentifierMockController < ActionController::API
+        #include Authorize::Token
+        #include Authorize::User
+        rescue_from ApiValidations::ApiError, with: :return_error_code
+        #before_action :validate_api_key
+        #before_action :validate_user
+        #before_action :validate_params
 
         def delete_additional_identifier
-          run_mock
-          result = @mock_controller.delete_additional_identifier
-          delete_response_result(result)
+          delete_organisation
+          render json: '', status: :ok
+        rescue StandardError
+          render json: '', status: :bad_request
         end
 
-        def remove_organisations_additional_identifier
-          @mock_controller = Api::V1::RemoveOrganisationsAdditionalIdentifierController.new
+        def validate_params
+          validate = ApiValidations::RemoveOrganisationAdditionalIdentifier.new(params)
+          render json: validate.errors, status: :bad_request unless validate.valid?
+        end
+
+        private
+
+        def delete_organisation
+          OrganisationSchemeIdentifier.find_by(ccs_org_id: params[:ccs_org_id].to_s,
+                                              scheme_org_reg_number: params[:identifier][:id].to_s,
+                                              scheme_code: params[:identifier][:scheme].to_s,
+                                              primary_scheme: false).destroy
+        end
+
+        def return_error_code(code)
+          render json: '', status: code.to_s
         end
       end
     end
