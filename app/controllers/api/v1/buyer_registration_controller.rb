cb0332dc6f52342fb4385b30e18944cae401f2f1
Tom Berey <tomberey1@gmail.com>
2021-06-10 16:20:14 +0100

    CON-998-Buyer-Reg-Migration-Endpoint

--- app/controllers/api/v1/buyer_registration_controller.rb
+++ app/controllers/api/v1/buyer_registration_controller.rb
@@ -12,7 +12,6 @@ module Api
         scheme_result = api_result
         additional_organisation if scheme_result.present?
 
-        @primary_added = false
         id_results = search_scheme_api
         coh_scheme_check(id_results)
         all_identifiers if defined?(id_results[:additionalIdentifiers])
@@ -38,8 +37,9 @@ module Api
           id_result[:additionalIdentifiers].each do |identifiers|
             add_primary_organisation(identifiers) if identifiers[:scheme] == Common::AdditionalIdentifier::SCHEME_COMPANIES_HOUSE
           end
+        else
+          add_primary_organisation(id_result[:identifier])
         end
-        add_primary_organisation(id_result[:identifier]) unless @primary_added ###################### work this into conditional****
       end
 
       def add_primary_organisation(identfier)
@@ -48,31 +48,17 @@ module Api
         organisation.scheme_org_reg_number = identfier[:id]
         organisation.uri = identfier[:uri]
         organisation.legal_name = identfier[:legalName]
-        organisation.ccs_org_id = @ccs_org_id
+        organisation.ccs_org_id = Common::GenerateId.ccs_org_id
         organisation.primary_scheme = true
         organisation.hidden = false
         organisation.client_id = Common::ApiHelper.find_client(api_key_to_string)
         organisation.save
-        @primary_added = true ######################################################################### ****
-      end
-
-      def add_additional_identifier(additional_identifier)
-        organisation = OrganisationSchemeIdentifier.new
-        organisation.scheme_code = additional_identifier[:scheme]
-        organisation.scheme_org_reg_number = additional_identifier[:id]
-        organisation.uri = additional_identifier[:uri]
-        organisation.legal_name = additional_identifier[:legalName]
-        organisation.ccs_org_id = @ccs_org_id
-        organisation.primary_scheme = false
-        organisation.hidden = false
-        organisation.client_id = Common::ApiHelper.find_client(api_key_to_string)
-        organisation.save
-        organisation.ccs_org_id
+        @ccs_org_id = organisation.ccs_org_id
       end
 
       def all_identifiers
         @all_identifier_api_result[:additionalIdentifiers].each do |identfier|
-          add_additional_identifier(identfier)
+          additional_organisation(identfier, false)
         end
       end
 
@@ -85,26 +71,17 @@ module Api
         additional_identifier_search_api_with_params.call
       end
 
-      # { ^^^^^^^returns^^^^^^^^^
-      #   name: name,
-      #   identifier: {CompaniesHouse::Indentifier.new(@result).build_response},
-      #   additionalIdentifiers: [{},{},{},...],
-      #   address: CompaniesHouse::Address.new(@result).build_response,
-      #   contactPoint: CompaniesHouse::Contact.new(@result).build_response
-      # }
-
-      def additional_organisation
+      def additional_organisation(identfier = @api_result, hidden = true)
         buyer_exists
         organisation = OrganisationSchemeIdentifier.new
-        organisation.scheme_code = @api_result[:scheme]
-        organisation.scheme_org_reg_number = @api_result[:id]
-        organisation.uri = @api_result[:uri]
-        organisation.legal_name = @api_result[:legalName]
-        organisation.ccs_org_id = Common::GenerateId.ccs_org_id
+        organisation.scheme_code = identfier[:scheme]
+        organisation.scheme_org_reg_number = identfier[:id]
+        organisation.uri = identfier[:uri]
+        organisation.legal_name = identfier[:legalName]
+        organisation.ccs_org_id = @ccs_org_id
         organisation.primary_scheme = false
-        organisation.hidden = true
+        organisation.hidden = hidden
         organisation.save
-        @ccs_org_id = organisation.ccs_org_id
       end
 
       def buyer_exists
@@ -116,13 +93,6 @@ module Api
         search_api_with_params = Salesforce::SalesforceBuyerRegistration.new(params[:account_id], params[:account_id_type])
         search_api_with_params.fetch_results
       end
-
-      # { ^^^^^^^returns^^^^^^^^^
-      #   scheme: Common::AdditionalIdentifier::SCHEME_CCS,
-      #   id: salesforce_scheme_id,
-      #   legalName: legal_name,
-      #   uri: uri,
-      # }
 
       def api_result
         @api_result = api_search_result
