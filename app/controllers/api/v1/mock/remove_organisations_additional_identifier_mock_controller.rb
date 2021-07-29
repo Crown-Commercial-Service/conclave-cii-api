86bd89f1a568ebd1adf716ff0a45c142257ea51b
Tom Berey <tomberey1@gmail.com>
2021-07-29 11:39:52 +0100

    CON-1201-Bugfix

--- app/controllers/api/v1/mock/remove_organisations_additional_identifier_mock_controller.rb
+++ app/controllers/api/v1/mock/remove_organisations_additional_identifier_mock_controller.rb
@@ -6,9 +6,10 @@ module Api
 
         def delete_additional_identifier
           run_mock
-          org = @mock_controller.find_organisation
-          @mock_controller.delete_additional_identifier if org.present?
-          delete_additional_response_result(org)
+          additional_org = @mock_controller.find_organisation
+          primary_org_check = @mock_controller.primary_org_check
+          @mock_controller.delete_additional_identifier if additional_org.present?
+          delete_additional_response_result(additional_org, primary_org_check)
         end
 
         def remove_additional_organisations
