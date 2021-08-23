9362b24c23d4360a7daeebd2a85f57e22215e288
Tom Berey <tomberey1@gmail.com>
2021-08-23 19:54:32 +0100

    CON-1214-Fix-Missing-Additional-Identifier-Add-Duplicate-Check

--- spec/services/salesforce/salesforce_buyer_registration_spec.rb
+++ spec/services/salesforce/salesforce_buyer_registration_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Salesforce::SalesforceBuyerRegistration, type: :services do
   describe 'build_arguments' do
     it 'Arguments with Salesforce ID' do
-      result = described_class.new('IWDBI382RI4B8OIU', 'SF-ID').build_arguments
+      result = described_class.new('IWDBI382RI4B8OIU', 'sfid').build_arguments
       expect(result).to eq("Id='IWDBI382RI4B8OIU'")
     end
 
     it 'Arguments with Salesforce URN' do
-      result = described_class.new('IWDBI382RI4B8OIU', 'SF-URN').build_arguments
+      result = described_class.new('IWDBI382RI4B8OIU', 'sfurn').build_arguments
       expect(result).to eq("Account_URN__c='IWDBI382RI4B8OIU'")
     end
   end
