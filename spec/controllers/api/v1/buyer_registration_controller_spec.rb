18ee951ee63964c5050b046a5edc9c5966f00bde
Tom Berey <tomberey1@gmail.com>
2021-09-05 18:32:33 +0100

    CON-1314-Fix-All-DM-Response-Issues

--- spec/controllers/api/v1/buyer_registration_controller_spec.rb
+++ spec/controllers/api/v1/buyer_registration_controller_spec.rb
@@ -49,6 +49,16 @@ RSpec.describe Api::V1::BuyerRegistrationController, type: :controller do
             }
           )
           .to_return(status: 404, body: '', headers: {})
+        stub_request(:get, "#{ENV['SALESFORCE_AUTH_URL']}/services/data/v45.0/query?q=SELECT%20ID,name,Status__c,Supplier_DUNS_Number__c,Company_Registration_Number__c,Account_URN__c%20FROM%20account%20WHERE%20Id='NSO7IUSHF98HFP9WEH9FFHZ'")
+          .with(
+            headers: {
+              'Accept'=>'*/*',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Authorization'=>'Bearer t07891Fbasibd60NM9rW9basidj49w7ig7R2S9',
+              'User-Agent'=>'Faraday v1.3.0'
+            }
+          )
+          .to_return(status: 200, body: "", headers: {})
       end
 
       # context 'when success sf-id' do
