6189af640fc23f2bbae58e2efa288621ae616a2a
Tom Berey <tomberey1@gmail.com>
2021-09-05 18:45:33 +0100

    CON-1314-Fix-All-DM-Response-Issues

--- spec/controllers/api/v1/buyer_registration_controller_spec.rb
+++ spec/controllers/api/v1/buyer_registration_controller_spec.rb
@@ -49,68 +49,26 @@ RSpec.describe Api::V1::BuyerRegistrationController, type: :controller do
             }
           )
           .to_return(status: 404, body: '', headers: {})
-        stub_request(:get, "#{ENV['SALESFORCE_AUTH_URL']}/services/data/v45.0/query?q=SELECT%20ID,name,Status__c,Supplier_DUNS_Number__c,Company_Registration_Number__c,Account_URN__c%20FROM%20account%20WHERE%20Id='NSO7IUSHF98HFP9WEH9FFHZ'")
-          .with(
-            headers: {
-              'Accept' => '*/*',
-              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Authorization' => 'Bearer t07891Fbasibd60NM9rW9basidj49w7ig7R2S9',
-              'User-Agent' => 'Faraday v1.3.0'
-            }
-          )
-          .to_return(status: 200, body: '', headers: {})
-        stub_request(:get, "#{ENV['SALESFORCE_AUTH_URL']}/services/data/v45.0/query?q=SELECT%20ID,name,Status__c,Supplier_DUNS_Number__c,Company_Registration_Number__c,Account_URN__c%20FROM%20account%20WHERE%20Account_URN__c='567345655679'")
-          .with(
-            headers: {
-              'Accept' => '*/*',
-              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-              'Authorization' => 'Bearer t07891Fbasibd60NM9rW9basidj49w7ig7R2S9',
-              'User-Agent' => 'Faraday v1.3.0'
-            }
-          )
-          .to_return(status: 200, body: '', headers: {})
       end
 
-      # context 'when success sf-id' do
-      #   it 'returns 201' do
-      #     post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
-      #     expect(response).to have_http_status(:created)
-      #     expect(response.status).to eq(201)
-      #     expect(response.body).to include('ccs_org_id')
-      #   end
-      # end
-
-      # context 'when success sf-urn' do
-      #   it 'returns 201' do
-      #     post :create_buyer, params: { account_id_type: 'SF-URN', account_id: '56734565567' }
-      #     expect(response).to have_http_status(:created)
-      #     expect(response.status).to eq(201)
-      #     expect(response.body).to include('ccs_org_id')
-      #   end
-      # end
+      context 'when success' do
+        it 'returns 201' do
+          post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
+          expect(response).to have_http_status(:created)
+        end
+      end
 
       # context 'when conflict' do
       #   it 'returns 409' do
       #     post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
       #     expect(response).to have_http_status(:conflict)
-      #     expect(response.status).to eq(409)
-      #     expect(response.body).to include('ccs_org_id')
       #   end
       # end
 
-      context 'when sf-id not found' do
+      context 'when not found' do
         it 'returns 404' do
-          post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFHZ' }
+          post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFH' }
           expect(response).to have_http_status(:not_found)
-          expect(response.status).to eq(404)
-        end
-      end
-
-      context 'when sf-urn not found' do
-        it 'returns 404' do
-          post :create_buyer, params: { account_id_type: 'SF-URN', account_id: '567345655679' }
-          expect(response).to have_http_status(:not_found)
-          expect(response.status).to eq(404)
         end
       end
 
@@ -118,7 +76,6 @@ RSpec.describe Api::V1::BuyerRegistrationController, type: :controller do
         it 'returns 404' do
           post :create_buyer, params: { account_id_type: 'sfurd', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
           expect(response).to have_http_status(:not_found)
-          expect(response.status).to eq(404)
         end
       end
 
@@ -126,7 +83,6 @@ RSpec.describe Api::V1::BuyerRegistrationController, type: :controller do
         it 'returns 404' do
           post :create_buyer, params: { account_id_type: '', account_id: '' }
           expect(response).to have_http_status(:not_found)
-          expect(response.status).to eq(404)
         end
       end
     end
@@ -134,26 +90,23 @@ RSpec.describe Api::V1::BuyerRegistrationController, type: :controller do
     context 'when invalid ApiKey' do
       it 'returns 401' do
         request.headers['x-api-key'] = 'invalid'
-        post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
+        post :create_buyer, params: { account_id_type: 'sfurd', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
         expect(response).to have_http_status(:unauthorized)
-        expect(response.status).to eq(401)
       end
     end
 
     context 'when invalid request header' do
       it 'returns 401' do
         request.headers['Apikey'] = ''
-        post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
+        post :create_buyer, params: { account_id_type: 'sfurd', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
         expect(response).to have_http_status(:unauthorized)
-        expect(response.status).to eq(401)
       end
     end
 
     context 'when no ApiKey' do
       it 'returns 401' do
-        post :create_buyer, params: { account_id_type: 'SF-ID', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
+        post :create_buyer, params: { account_id_type: 'sfurd', account_id: 'NSO7IUSHF98HFP9WEH9FFG' }
         expect(response).to have_http_status(:unauthorized)
-        expect(response.status).to eq(401)
       end
     end
   end
