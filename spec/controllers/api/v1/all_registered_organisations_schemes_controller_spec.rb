3c064e8ff3829c872f2865a0ee8a7f8bf5165537
Tom Berey <tomberey1@gmail.com>
2021-11-10 14:46:39 +0000

    CON-1618-Update-Auth-Endpoints

--- spec/controllers/api/v1/all_registered_organisations_schemes_controller_spec.rb
+++ spec/controllers/api/v1/all_registered_organisations_schemes_controller_spec.rb
@@ -47,10 +47,9 @@ RSpec.describe Api::V1::AllRegisteredOrganisationsSchemesController, type: :cont
       end
 
       context 'when invalid params' do
-        let(:jwt_token) { JWT.encode({ roles: '', ciiOrgId: '', aud: '' }, '') }
-        it 'returns 401' do
+        it 'returns 404' do
           get :search_organisation, params: { ccs_org_id: 'null' }
-          expect(response).to have_http_status(:unauthorized)
+          expect(response).to have_http_status(:not_found)
         end
       end
     end
