3fda25cebaeb465b9f7b3813242c7044bcca8209
tberey <thomas.berey@crowncommercial.gov.uk>
2021-12-13 12:45:40 +0000

    CON-1678-Revert-Changes-And-Add-Address-Response

--- spec/controllers/api/v1/registered_organisations_schemes_controller_spec.rb
+++ spec/controllers/api/v1/registered_organisations_schemes_controller_spec.rb
@@ -42,9 +42,9 @@ RSpec.describe Api::V1::RegisteredOrganisationsSchemesController, type: :control
       end
 
       context 'when invalid params' do
-        it 'returns 404' do
+        it 'returns 400' do
           get :search_organisation, params: { ccs_org_id: '', clientid: clientid }
-          expect(response).to have_http_status(:not_found)
+          expect(response).to have_http_status(:bad_request)
         end
       end
     end
