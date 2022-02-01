da36d860c98893a23aa8f01e8b4cb142b9aa3ae1
tberey <thomas.berey@crowncommercial.gov.uk>
2022-02-01 11:08:43 +0000

    CON-1322-1909-1911-NHS-Registry-Serializer-Status-Codes

--- spec/controllers/api/v1/remove_organisations_additional_identifier_controller_spec.rb
+++ spec/controllers/api/v1/remove_organisations_additional_identifier_controller_spec.rb
@@ -44,14 +44,14 @@ RSpec.describe Api::V1::RemoveOrganisationsAdditionalIdentifierController, type:
 
       context 'when invalid id' do
         it 'returns 404' do
-          delete :delete_additional_identifier, params: { ccs_org_id: organisation_scheme_identifier.ccs_org_id, id: 'invalid', scheme: scheme_register.scheme_register_code }
+          delete :delete_additional_identifier, params: { ccs_org_id: 342678251, id: 'invalid', scheme: 'GB-COH' }
           expect(response).to have_http_status(:not_found)
         end
       end
 
       context 'when invalid scheme' do
         it 'returns 404' do
-          delete :delete_additional_identifier, params: { ccs_org_id: organisation_scheme_identifier.ccs_org_id, id: ccs_org_id, scheme: 'invalid' }
+          delete :delete_additional_identifier, params: { ccs_org_id: 342678251, id: 101123, scheme: 'invalid' }
           expect(response).to have_http_status(:not_found)
         end
       end
