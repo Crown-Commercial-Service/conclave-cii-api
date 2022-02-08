7a521e10d2253862b1187916e531549c454487ca
tberey <thomas.berey@crowncommercial.gov.uk>
2022-02-08 12:49:47 +0000

    CON-2009-2013-2019-NHS-Registry-Service-Updates-And-Access-Token-Test

--- app/services/nhs/address.rb
+++ app/services/nhs/address.rb
@@ -9,21 +9,29 @@ module Nhs
       {
         streetAddress: street_address,
         locality: locality,
-        region: '',
+        region: region,
         postalCode: postal_code,
         countryName: country_name
       }
     end
 
     def street_address
-      "#{exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn1'])}#{street_address_two}"
+      "#{exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn1'])}#{street_address_two}#{street_address_three}"
     end
 
     def street_address_two
       ", #{exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn2'])}" if exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn2']).present?
     end
 
+    def street_address_three
+      ", #{exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn3'])}" if exists_or_null(@result['Organisation']['GeoLoc']['Location']['AddrLn3']).present?
+    end
+
     def locality
+      exists_or_null(@result['Organisation']['GeoLoc']['Location']['Town'])
+    end
+
+    def region
       exists_or_null(@result['Organisation']['GeoLoc']['Location']['County'])
     end
 
--- app/services/nhs/contact.rb
+++ app/services/nhs/contact.rb
@@ -9,10 +9,37 @@ module Nhs
       {
         name: '',
         email: '',
-        telephone: '',
-        faxNumber: '',
-        uri: ''
+        telephone: exists_or_null(telephone),
+        faxNumber: exists_or_null(fax),
+        uri: exists_or_null(uri)
       }
+    end
+
+    def telephone
+      "#{get_contact_value(@result['Organisation']['Contacts']['Contact'], 'tel')}" if @result['Organisation']['Contacts'].present?
+    end
+
+    def fax
+      "#{get_contact_value(@result['Organisation']['Contacts']['Contact'], 'fax')}" if @result['Organisation']['Contacts'].present?
+    end
+
+    def uri
+      "#{get_contact_value(@result['Organisation']['Contacts']['Contact'], 'http')}" if @result['Organisation']['Contacts'].present?
+    end
+
+    private
+
+    def get_contact_value(contacts, type)
+      return '' unless contacts.present?
+
+      contacts.each do |contact|
+        return contact['value'] if contact['type'] == type.to_s
+      end
+      ''
+    end
+
+    def exists_or_null(api_param)
+      api_param.present? ? api_param : ''
     end
   end
 end
