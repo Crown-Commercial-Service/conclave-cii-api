0a203db2314b9baed932ed0841a02d968032943f
tberey <thomas.berey@crowncommercial.gov.uk>
2022-03-24 16:19:53 +0000

    CON-1555-2166-Remove-Old-Vault-Config-and-Update-Access-Token-Controller

--- config/initializers/aws_ssm.rb
+++ config/initializers/aws_ssm.rb
@@ -8,18 +8,19 @@ def config_aws
 	vcap_services['user-provided'].each do |user_service|
 		@params_list = user_service['credentials']['VARS_LIST'] if user_service['credentials']['VARS_LIST'].present?
 		if user_service['credentials']['aws_access_key_id'].present?
-			@ssm_client = Aws::SSM::Client.new(
+			ssm_client = Aws::SSM::Client.new(
 				region: user_service['credentials']['region'],
 				access_key_id: user_service['credentials']['aws_access_key_id'],
 				secret_access_key: user_service['credentials']['aws_secret_access_key']
 			)
+			set_env(ssm_client)
 		end
 	end
 end
 
-def set_env
+def set_env(ssm_client)
 	@params_list.each do |param_name|
-		ENV[param_name] = @ssm_client.get_parameter({ name: "/conclave-cii/#{param_name}", with_decryption: true })[:parameter][:value]
+		ENV[param_name] = ssm_client.get_parameter({ name: "/conclave-cii/#{param_name}", with_decryption: true })[:parameter][:value]
 	end
 	config_rollbar if ENV['ROLLBAR_ACCESS_TOKEN'].present?
 end
