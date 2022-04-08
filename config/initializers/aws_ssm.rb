6f145a23afbe7da6974b9539750b1e3dfe20f23c
@tberey <tomberey1@gmail.com>
2022-04-08 21:06:40 +0100

    CON-1555-Refine-AWS-SSM-Config

--- config/initializers/aws_ssm.rb
+++ config/initializers/aws_ssm.rb
@@ -3,24 +3,26 @@ require 'rollbar'
 
 private
 
-#New SSM Config, replacing Hashicorp Vault.
 def config_aws
+	params_list = []
+	ssm_client = nil
 	vcap_services = JSON.parse(ENV['VCAP_SERVICES'])
+
 	vcap_services['user-provided'].each do |user_service|
-		@params_list = user_service['credentials']['VARS_LIST'] if user_service['credentials']['VARS_LIST'].present?
+		params_list = user_service['credentials']['VARS_LIST'] if user_service['credentials']['VARS_LIST'].present?
 		if user_service['credentials']['aws_access_key_id'].present?
 			ssm_client = Aws::SSM::Client.new(
 				region: user_service['credentials']['region'],
 				access_key_id: user_service['credentials']['aws_access_key_id'],
 				secret_access_key: user_service['credentials']['aws_secret_access_key']
 			)
-			set_env(ssm_client)
 		end
 	end
+	set_env(ssm_client, params_list) if ssm_client && params_list.any?
 end
 
-def set_env(ssm_client)
-	@params_list.each do |param_name|
+def set_env(ssm_client, params_list)
+	params_list.each do |param_name|
 		ENV[param_name] = ssm_client.get_parameter({ name: "/conclave-cii/#{param_name}", with_decryption: true })[:parameter][:value]
 	end
 	config_rollbar if ENV['ROLLBAR_ACCESS_TOKEN'].present?
