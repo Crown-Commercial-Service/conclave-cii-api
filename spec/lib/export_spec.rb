30c9ae70ba84f3f4aef0ca4e2433ff3cc1506bfe
@tberey <tomberey1@gmail.com>
2022-05-24 18:15:10 +0100

    data-export-job

--- spec/lib/export_spec.rb
+++ spec/lib/export_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require export
 
-describe "export" do
-    it 'tests it' do
-        expect(Export.success).to eq(true)
-    end
+describe 'Export' do
+  it 'tests it' do
+    expect(Export.success).to eq(true)
+  end
 end
