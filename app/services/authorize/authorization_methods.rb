12b3697c0b74feb51236244f26e641485f9f22cf
@tberey <tomberey1@gmail.com>
2022-07-28 13:58:58 +0100

    CON-1373-Relax-Org-ID-Check-On-MSE-Role-For-Registered-Orgs

--- app/services/authorize/authorization_methods.rb
+++ app/services/authorize/authorization_methods.rb
@@ -60,7 +60,7 @@ module Authorize
       validate_client_id
       validate_user_access_token
       validate_access_token
-      validate_service_eligibility_or_ccs_admin_user
+      validate_service_eligibility_or_ccs_admin_user(validate_and_decode_token)
     end
   end
 end
--- app/services/authorize/user.rb
+++ app/services/authorize/user.rb
@@ -41,13 +41,12 @@ module Authorize
       ApiValidations::ApiErrorValidationResponse.new(:user_access_unauthorized) unless decoded_token[0]['roles'].include?(ENV['ACCESS_MANAGE_SUBSCRIPTIONS'])
     end
 
-    def validate_service_eligibility_or_ccs_admin_user
-      decoded_token = validate_and_decode_token
+    def validate_service_eligibility_or_ccs_admin_user(decoded_token)
       if decoded_token[0]['roles'].include?(ENV.fetch('ACCESS_MANAGE_SUBSCRIPTIONS', nil)) || decoded_token[0]['roles'].include?(ENV.fetch('ACCESS_ORGANISATION_ADMIN', nil))
-        validate_ccs_org_id unless decoded_token[0]['roles'].include?(ENV.fetch('ACCESS_MANAGE_SUBSCRIPTIONS', nil))
-      else
-        ApiValidations::ApiErrorValidationResponse.new(:user_access_unauthorized)
+        return validate_ccs_org_id unless decoded_token[0]['roles'].include?(ENV.fetch('ACCESS_MANAGE_SUBSCRIPTIONS', nil))
+
       end
+      ApiValidations::ApiErrorValidationResponse.new(:user_access_unauthorized)
     end
 
     def registered_orgs_controller_role_check
