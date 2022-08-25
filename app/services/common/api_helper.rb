10c0d3bd5adbe51f18d27aa143716b053bbd1a67
@tberey <tomberey1@gmail.com>
2022-08-25 16:11:16 +0100

    CON-2784-Fix-Test-Identifier-DM-Bug

--- app/services/common/api_helper.rb
+++ app/services/common/api_helper.rb
@@ -117,7 +117,7 @@ module Common
           scheme: scheme,
           id: Common::AdditionalIdentifier::MOCK_ID,
           legalName: 'Nicks Testing Organisation',
-          uri: ''
+          uri: 'https://www.example.com/'
         },
         additionalIdentifiers: [],
         address: {
@@ -128,11 +128,11 @@ module Common
           countryName: 'England'
         },
         contactPoint: {
-          name: '',
-          email: '',
+          name: 'Name',
+          email: 'email@test.com',
           telephone: '01234567890',
-          faxNumber: '',
-          uri: ''
+          faxNumber: '01234567890',
+          uri: 'https://www.example.com/'
         }
       }
     end
