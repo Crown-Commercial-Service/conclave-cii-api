bcc9f8d6551e6f1fb6a7950a8d8edba6327d01d0
mo-zag <mo-zag@users.noreply.github.com>
2023-01-18 20:32:00 +0000

    Created new registry duns api
    Search duns api for companies house registration number

--- app/services/search_api.rb
+++ app/services/search_api.rb
@@ -30,7 +30,9 @@ class SearchApi
 
   def get_companies_house(company_reg_number)
     company_api = CompaniesHouse::Search.new(company_reg_number)
-    company_api.fetch_results
+    results = company_api.fetch_results
+    results[:additionalIdentifiers] = get_addtional_identfiers(results[:additionalIdentifiers]) if results.present?
+    results
   end
 
   def get_charity(charity_number, scheme_id)
@@ -40,8 +42,8 @@ class SearchApi
     results
   end
 
-  def get_duns(dnd_number)
-    dnb = Dnb::Search.new(dnd_number)
+  def get_duns(duns_number)
+    dnb = Dnb::Search.new(duns_number)
     results = dnb.fetch_results
     results[:additionalIdentifiers] = get_addtional_identfiers(results[:additionalIdentifiers]) if results.present?
     results
@@ -50,6 +52,11 @@ class SearchApi
   def get_nhs(organisation_code)
     nhs = Nhs::Search.new(organisation_code)
     nhs.fetch_results
+  end
+
+  def get_duns_coh(company_reg_number)
+    dnb_chn = DnbChn::Search.new(company_reg_number)
+    dnb_chn.fetch_results
   end
 
   def get_addtional_identfiers(identfiers)
