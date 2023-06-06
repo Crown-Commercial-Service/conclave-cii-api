627184ff2718fd7b8bd8602fd6a0202ed054cf39
mohammed Aziz <mo_zag@yahoo.co.uk>
2023-06-06 13:16:45 +0100

    Update aws_ssm.rb

--- config/initializers/aws_ssm.rb
+++ config/initializers/aws_ssm.rb
@@ -32,7 +32,7 @@ def config_rollbar
 	Rollbar.configure do |config|
 	  
 		config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-		config.environment = ENV['SERVER_ENV_NAME']
+		config.environment = ENV['ROLLBAR_ENVIRONMENT']
 	  
 	end
 	Rails.logger.info('App Deployed & Rollbar Successfully Configured')
