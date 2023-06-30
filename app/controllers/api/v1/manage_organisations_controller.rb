d7eae3df880bd5be0a5db106274cc6df0feeae24
@tberey <tomberey1@gmail.com>
2023-06-30 13:47:48 +0100

    PPG-344-Add-Address-Back-Into-Incorrect-Endpoint

--- app/controllers/api/v1/manage_organisations_controller.rb
+++ app/controllers/api/v1/manage_organisations_controller.rb
@@ -9,7 +9,7 @@ module Api
       before_action :validate_params
 
       def search_organisation
-        scheme_result = api_result.except(:address, :contactPoint)
+        scheme_result = api_result
         # While PPON is being developed, we want any related CII functionality to be inaccessible temporarily.
         if scheme_result.blank? || params[:scheme].upcase == Common::AdditionalIdentifier::SCHEME_PPON
           render json: '', status: :not_found
