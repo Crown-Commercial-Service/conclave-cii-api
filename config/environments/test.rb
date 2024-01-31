0fd714e803d7dd040a4618027b3452df173cb2bc
mohammed Aziz <mo_zag@yahoo.co.uk>
2024-01-31 03:46:43 +0000

    updated config for cloudwatch

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,6 +45,12 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # end
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
