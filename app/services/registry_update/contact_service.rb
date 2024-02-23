324c51e40509a1bc5000b00192dc5b75c7231da8
mohammed Aziz <mo_zag@yahoo.co.uk>
2024-02-23 14:09:37 +0000

    Update contact_service.rb

--- app/services/registry_update/contact_service.rb
+++ app/services/registry_update/contact_service.rb
@@ -13,7 +13,7 @@ module RegistryUpdate
       resp = update_org_contact_details(@conn)
       # ApiLogging::Logger.error("CONTACT SERVICE API| method:connect_contact_api, #{resp.to_json}") if resp.status != 200
     rescue StandardError => e
-      ApiLogging::Logger.fatal("CONTACT SERVICE API| method:connect_contact_api, #{e.to_json}")
+      # ApiLogging::Logger.fatal("CONTACT SERVICE API| method:connect_contact_api, #{e.to_json}")
     end
 
     def get_org_contact_details(contact_conn)
