6f5650ff699a3efd81ebf9528507cbe38966f992
mo-zag <mo-zag@users.noreply.github.com>
2026-06-03 21:00:06 +0100

    test if vars are set during CII run

--- spec/controllers/api/v1/create_organisations_controller_spec.rb
+++ spec/controllers/api/v1/create_organisations_controller_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Api::V1::CreateOrganisationsController do
     let(:organisation_id) { nil }
     let(:jwt_token) { JWT.encode({ roles: ENV.fetch('ACCESS_ORGANISATION_ADMIN', nil), ciiOrgId: organisation_id, aud: ENV.fetch('CLIENT_ID', nil) }, 'test') }
 
-    after do
-      WebMock::RequestRegistry.instance.requested_signatures.hash.each_key do |request_signature|
-        puts "WebMock request made: #{request_signature}"
-      end
-    end
+    # after do
+    #   WebMock::RequestRegistry.instance.requested_signatures.hash.each_key do |request_signature|
+    #     puts "WebMock request made: #{request_signature}"
+    #   end
+    # end
 
     context 'when success' do
       before do
