36ab16aabbb8d6641e8b06bd6a9356eebd78f1de
mo-zag <mo-zag@users.noreply.github.com>
2026-06-08 13:38:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Run the rake task to update gca changes only a fix for lower env and will remove
-echo "Running database gca changes"
-bundle exec rake ccs_to_gca:update_seeds
+# echo "Running database gca changes"
+# bundle exec rake ccs_to_gca:update_seeds
 
 echo "Starting Rails Server..."
 exec "$@"
